eec868c82c9d6863ca6d5cc309c5b4e967df1e3f
trappycorp <160514727+trappycorp@users.noreply.github.com>
2024-02-20 14:37:43 +0530

    added site

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+const body = document.querySelector("body"),
+    modeToggle = body.querySelector(".mode-toggle");
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+
+
+
+
+
+
+//Search Code
+const search = document.querySelector(".search-box input"),
+      images = document.querySelectorAll(".image-box");
+
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        let searcValue = search.value,
+            value = searcValue.toLowerCase();
+            images.forEach(image =>{
+                if(value === image.dataset.name){
+                    return image.style.display = "block";
+                }
+                image.style.display = "none";
+            });
+    }
+});
+
+search.addEventListener("keyup", () =>{
+    if(search.value !="") return;
+
+    images.forEach(image => {
+        image.style.display = "block";
+    })
+})
